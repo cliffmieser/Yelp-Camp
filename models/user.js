8710beb9b7d59efebd8a2945dab46bcf064e5ecc
cliffmieser <cliffmieser@gmail.com>
2023-06-01 18:13:21 -0400

    post route to register form added

--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ const passportLocalMongoose = require('passport-local-mongoose');
 
 const UserSchema = new Schema({
     email: {
-        type: string,
+        type: String,
         required: true,
         unique: true
     }
