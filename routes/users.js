e48b968ceec04e3caf2971a86584c5049df38bb5
cliffmieser <cliffmieser@gmail.com>
2023-06-01 21:55:21 -0400

    users are now redirected to original url request upon login

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require('../models/user');
 const catchAsync = require('../utils/catchAsync');
 const passport = require('passport');
+const {storeReturnTo} = require('../middleware');
 
 router.get('/register', (req, res) =>{
     res.render('users/register');
@@ -32,9 +33,11 @@ router.get('/login', (req, res) =>{
 })
 
 //passport authenticate middleware that uses the local strategy with failure options
-router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), (req, res) =>{
+router.post('/login', storeReturnTo, passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), (req, res) =>{
         req.flash('success', 'welcome back!');
-        res.redirect("/campgrounds");
+        const redirectUrl = res.locals.returnTo || "/campgrounds";
+        delete req.session.returnTo;
+        res.redirect(redirectUrl);
 })
 
 router.get('/logout', (req, res, next)=>{
