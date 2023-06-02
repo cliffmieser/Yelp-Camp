c97deb6781cc2d8c1a3116de23b4a12c803befab
cliffmieser <cliffmieser@gmail.com>
2023-06-01 20:32:38 -0400

    passport authentication added

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 const catchAsync = require('../utils/catchAsync');
+const passport = require('passport');
 
 router.get('/register', (req, res) =>{
     res.render('users/register');
@@ -24,7 +25,14 @@ router.post('/register', catchAsync(async(req, res) =>{
     }
 }));
 
+router.get('/login', (req, res) =>{
+    res.render('users/login')
+})
 
-
+//passport authenticate middleware that uses the local strategy with failure options
+router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), (req, res) =>{
+        req.flash('success', 'welcome back!');
+        res.redirect("/campgrounds");
+})
 
 module.exports = router;
