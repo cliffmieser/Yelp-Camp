c996e1e2077178facb50ec4ea1853b66f53126c1
cliffmieser <cliffmieser@gmail.com>
2023-06-01 20:17:46 -0400

    registration route logic added

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const catchAsync = require('../utils/catchAsync');
 
 router.get('/register', (req, res) =>{
     res.render('users/register');
 });
 
-router.post('/register', async(req, res) =>{
-    res.send(req.body);
-})
+router.post('/register', catchAsync(async(req, res) =>{
+    try {
+    const {email, username, password} = req.body;
+    const user = new User({email, username});
+    const registeredUser = await User.register(user, password);
+    console.log(registeredUser);
+    req.flash('success', 'welcome to YelpCamp!');
+    res.redirect('/campgrounds');
+    }catch (e) {
+        let {existingUser} = e.message;
+        existingUser = `A user with the given username: "${req.body.username}"
+            already exists! `;
+        req.flash('error', existingUser);
+        res.redirect('/register');
+    }
+}));
+
 
 
 
