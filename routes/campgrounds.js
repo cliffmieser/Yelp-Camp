12f5f647e412d6d735495f4ce5117274c56534f3
cliffmieser <cliffmieser@gmail.com>
2023-06-02 21:22:22 -0400

    user authorization logic added

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -30,6 +30,7 @@ router.get('/new', isLoggedIn, (req, res) => {
 router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, next) =>{
         /* if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400); */
         const campground = new Campground(req.body.campground);
+        campground.author = req.user._id;
         await campground.save();
         req.flash('success', 'Successfully made a campground!');
         res.redirect(`/campgrounds/${campground._id}`);
@@ -39,7 +40,8 @@ router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, nex
 
 
 router.get('/:id', catchAsync(async (req, res) =>{
-    const campground = await Campground.findById(req.params.id).populate('reviews');
+    const campground = await Campground.findById(req.params.id).populate('reviews').populate('author');
+    console.log(campground);
     if(!campground){
         req.flash('error', 'Campground does not exit. :(');
         return res.redirect('/campgrounds');
@@ -48,19 +50,27 @@ router.get('/:id', catchAsync(async (req, res) =>{
 }))
 
 router.get('/:id/edit', isLoggedIn, catchAsync(async(req, res) => {
+    const {id} = req.params;
     const campground = await Campground.findById(req.params.id);
     if(!campground){
         req.flash('error', 'Campground does not exit. :(');
         return res.redirect('/campgrounds');
     }
+    if(!campground.author.equals(req.user._id)){
+        req.flash('error','You do not have permission to do that.');
+        return res.redirect(`/campgrounds/${id}`);
+    }
     res.render('campgrounds/edit', {campground});
 }))
 
-
-
 router.put('/:id', isLoggedIn, validateCampground, catchAsync(async(req, res) =>{
-    const {id} = req.params
-    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
+    const {id} = req.params;
+    const campground = await Campground.findById(id);
+    if(!campground.author.equals(req.user._id)){
+        req.flash('error','You do not have permission to do that.');
+        return res.redirect(`/campgrounds/${id}`);
+    }
+    const camp = await Campground.findByIdAndUpdate(id, {...req.body.campground})
     req.flash('success', 'Successfully updated campground!')
     res.redirect(`/campgrounds/${campground._id}`);
 }))
@@ -73,3 +83,4 @@ router.delete('/:id', catchAsync(async(req, res) =>{
 }))
 
 module.exports = router;
+
