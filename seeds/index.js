b0fb96ab34ac9d1660e66a0bb496622d367f25d1
cliffmieser <cliffmieser@gmail.com>
2023-06-02 21:22:49 -0400

    seed data added to test authorization

--- seeds/index.js
+++ seeds/index.js
@@ -20,6 +20,7 @@ const seedDB = async() => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 30) + 10;
         const camp = new Campground({
+            author: "647a94c80f8d9391c24a4e10",
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: "https://source.unsplash.com/collection/482351",
