75c9b1dfeb7e89de19e53f7bbc98a9a5e6bdf06f
cliffmieser <cliffmieser@gmail.com>
2023-06-19 21:07:17 -0400

    code refactored into controller directory

---
+++ controllers/users.js
@@ -0,0 +1,38 @@
+
+
+module.exports.renderRegister = (req, res) =>{
+    res.render('users/register');
+}
+
+module.exports.register = async(req, res, next) =>{
+    try{
+        const {email, username, password} = req.body;
+        const user = new UserActivation({email, username});
+        const registeredUser = await UserActivation.register(user, password);
+        req.login(registeredUser, err => {
+            if(err) return next(err);
+            req.flash('success', 'Welcome to Yelp Camp!');
+            res.redirect('/campgrounds');
+        })
+    } catch (e){
+        res.flash('error', e.message);
+        res.redirect('register');
+    }
+}
+
+module.exports.renderLogin =  (req, res) =>{
+    res.render('users/login');
+}
+
+module.exports.login = (req, res) =>{
+    req.flash('success', 'welcome back!');
+    const redirectUrl = req.session.returnto || '/campgrounds';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
+}
+
+module.exports.logout = (req, res) =>{
+    req.logout();
+    req.flash('success', 'Goodbye!');
+    res.redirect('/campgrounds');
+}
