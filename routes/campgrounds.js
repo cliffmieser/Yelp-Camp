bbd3443a1323567de6514a2bc5895e69a3adbb8b
cliffmieser <cliffmieser@gmail.com>
2023-07-20 21:43:26 -0400

    router modules added

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -24,21 +24,5 @@ router.route('/:id')
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm))
 
 
-
-// router.get('/', catchAsync(campgrounds.index));
-
-// router.get('/new', isLoggedIn, campgrounds.renderNewForm)
-
-
-// router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
-
-// router.get('/:id', catchAsync(campgrounds.showCampground))
-
-// router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm))
-
-// router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
-
-// router.delete('/:id',isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground))
-
 module.exports = router;
 
