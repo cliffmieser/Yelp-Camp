31213c1dd92601295934f12805970128f6425732
cliffmieser <cliffmieser@gmail.com>
2023-07-23 20:07:16 -0400

    multer package added

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -6,12 +6,17 @@ const { campgroundSchema} = require('../schemas.js');
 const catchAsync = require('../utils/catchAsync');
 const {isLoggedIn, isAuthor, validateCampground} = require("../middleware");
 
+const multer = require("multer");
+const upload = multer({dest: 'uploads/'});
 
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
-
+    // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
+    .post(upload.array('image') ,(req,res) =>{
+        console.log(req.body, req.file);
+        res.send("success");
+    })
 router.get('/new', isLoggedIn, campgrounds.renderNewForm)
 
 
