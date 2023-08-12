78279bb60b18d3a9e75684f8afcdcaf0fc0648e5
cliffmieser <cliffmieser@gmail.com>
2023-08-12 18:41:45 -0400

    satellite streets map render

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -3,7 +3,7 @@ mapboxgl.accessToken = mapToken;
 
 const map = new mapboxgl.Map({
 container: 'map', // container ID
-style: 'mapbox://styles/mapbox/streets-v12', // style URL
+style: 'mapbox://styles/mapbox/satellite-streets-v12', // style URL
 center: campground.geometry.coordinates, // starting position [lng, lat]
 zoom: 10, // starting zoom
 });
@@ -11,4 +11,11 @@ zoom: 10, // starting zoom
 
 new mapboxgl.Marker()
 .setLngLat(campground.geometry.coordinates)
+.setPopup(
+    new mapboxgl.Popup({offset: 25})
+    .setHTML(
+        `<h3>${campground.title}</h3>
+        <p>${campground.location}</p>`
+    )
+)
 .addTo(map);
