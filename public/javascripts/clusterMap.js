6657cadafa492590e575c5791f2205afd909c5d2
cliffmieser <cliffmieser@gmail.com>
2023-08-15 15:02:11 -0400

    map popup links to focused campground

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -44,20 +44,20 @@ paint: {
 'circle-color': [
 	'step',
 	['get', 'point_count'],
-	'#51bbd6',
+	'#009688',
 	100,
-	'#f1f075',
+	'#00796B',
 	750,
-	'#f28cb1'
+	'#004D40'
 	],
 	'circle-radius': [
 	'step',
 	['get', 'point_count'],
+	15,
+	10,
 	20,
-	100,
 	30,
-	750,
-	40
+	25
 	]
 	}
 	});
@@ -111,25 +111,23 @@ map.addLayer({
 // the location of the feature, with
 // description HTML from its properties.
 map.on('click', 'unclustered-point', (e) => {
-const coordinates = e.features[0].geometry.coordinates.slice();
-const mag = e.features[0].properties.mag;
-const tsunami =
-e.features[0].properties.tsunami === 1 ? 'yes' : 'no';
- 
-// Ensure that if the map is zoomed out such that
-// multiple copies of the feature are visible, the
-// popup appears over the copy being pointed to.
-while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-}
- 
-new mapboxgl.Popup()
-.setLngLat(coordinates)
-.setHTML(
-`magnitude: ${mag}<br>Was there a tsunami?: ${tsunami}`
-)
-.addTo(map);
-});
+	const {popUpMarkup} =  e.features[0].properties;
+	const coordinates = e.features[0].geometry.coordinates.slice();
+	
+	// Ensure that if the map is zoomed out such that
+	// multiple copies of the feature are visible, the
+	// popup appears over the copy being pointed to.
+	while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+		coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+		}
+	
+	new mapboxgl.Popup()
+	.setLngLat(coordinates)
+	.setHTML(
+		popUpMarkup
+	)
+	.addTo(map);
+	});
  
 
 //events for mousing over or out of clusters
