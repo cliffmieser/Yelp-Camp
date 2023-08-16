32b0989ff4593a36f4c1eba5552f71c39ef48042
cliffmieser <cliffmieser@gmail.com>
2023-08-15 23:59:05 -0400

    Inline map styles TODO: Display map on show and index page

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -7,7 +7,7 @@ mapboxgl.accessToken = mapToken;
 
 //creates map & map container
 const map = new mapboxgl.Map({
-container: 'map',
+container: 'cluster-map',
 // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
 style: 'mapbox://styles/mapbox/dark-v11',
 center: [-103.5917, 40.6699],
